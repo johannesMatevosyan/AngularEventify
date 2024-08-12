372dc9d8c35ea970dfd379afbecd50105090e5d2
Hovhannes Matevosyan <net.hovhannes.matevosyan@mhp.com>
2024-08-12 15:32:34 +0400

    developed features to generate time slots based on daily time hours

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -12,36 +12,43 @@ export class CalendarComponent implements OnInit {
   endOfWeek = this.now.endOf('week');
   weekStart = this.startOfWeek.toFormat("ccc, dd");
   weekEnd = this.endOfWeek.toFormat("ccc, dd");
-  year = this.now.year;
-  monthName = this.startOfWeek.monthLong;
+  timeFrame = {
+    month: this.startOfWeek.monthLong,
+    year: this.now.year
+  }
+  timeSlots: string[] = [];
   weekDates: string[] = [];
+  startHour = 6; // 06:00 AM
+  endHour = 19;  // 07:00 PM
   ngOnInit(): void {
-
-    this.weekDates = Array.from({ length: 7 }, (_, i) => {
-      return this.startOfWeek.plus({ days: i }).toFormat('ccc, dd');
-    });
+    this.weekDates = this.getWeekDates(this.startOfWeek);
+    this.timeSlots = this.incrementDailyTime(this.startHour, this.endHour);
   }
   getToday(): void {
     this.startOfWeek = this.now.startOf('week');
     this.endOfWeek = this.now.endOf('week');
     this.weekStart = this.startOfWeek.toFormat("ccc, dd");
     this.weekEnd = this.endOfWeek.toFormat("ccc, dd");
-    this.year = this.now.year;
-    this.monthName = this.startOfWeek.monthLong;
-    this.weekDates = Array.from({ length: 7 }, (_, i) => {
-      return this.startOfWeek.plus({ days: i }).toFormat('ccc, dd');
-    });
+    // get month name of the current year
+    this.timeFrame.year = this.now.year;
+    this.timeFrame.month = this.startOfWeek.monthLong;
+    // Create an array of dates for the first week of the year
+    this.weekDates = this.getWeekDates(this.startOfWeek);
   }
-  getWeekChange(event$: string): void {
-    this.startOfWeek = event$ === 'previous' ? this.startOfWeek.minus({ weeks: 1 }) : this.startOfWeek.plus({ weeks: 1 });
-    this.monthName = this.monthName === this.startOfWeek.monthLong ? this.monthName : this.startOfWeek.monthLong ;
-    this.year = this.year === this.startOfWeek.year ? this.year : this.startOfWeek.year;
+  getWeekChange(event$: 'previous'|'next'): void {
+    this.startOfWeek = this.detectWeekChange(event$);
+    // get month name of the current year
+    this.timeFrame.month = this.checkMonth(this.startOfWeek.monthLong);
+    this.timeFrame.year = this.checkYear(this.startOfWeek.year);
+
     this.endOfWeek = this.startOfWeek.endOf('week');
     this.weekStart = this.startOfWeek.toFormat("ccc, dd");
     this.weekEnd = this.endOfWeek.toFormat("ccc, dd");
-    this.weekDates = Array.from({ length: 7 }, (_, i) =>
-      this.startOfWeek.plus({ days: i }).toFormat('ccc, dd')
-    );
+    // Create an array of dates for the first week of the year
+    this.weekDates = this.getWeekDates(this.startOfWeek);
+  }
+  detectWeekChange(event$: 'previous'|'next'): DateTime<true> {
+    return event$ === 'previous' ? this.startOfWeek.minus({ weeks: 1 }) : this.startOfWeek.plus({ weeks: 1 });
   }
   getFirstOrLastWeek(event$: 'first'|'last'): void {
     // Get the first or last day of the current year
@@ -51,13 +58,42 @@ export class CalendarComponent implements OnInit {
     this.startOfWeek = resultWeek.startOf('week');
 
     // Get the first or last month name of the the year
-    this.monthName = this.startOfWeek.monthLong;
+    this.timeFrame.month = this.startOfWeek.monthLong;
     this.weekStart = this.startOfWeek.toFormat("ccc, dd");
     this.endOfWeek = this.startOfWeek.endOf('week');
     this.weekEnd = this.endOfWeek.toFormat("ccc, dd");
     // Create an array of dates for the first week of the year
-    this.weekDates = Array.from({ length: 7 }, (_, i) =>
-      this.startOfWeek.plus({ days: i }).toFormat('ccc, dd') // Format as desired
+    this.weekDates = this.getWeekDates(this.startOfWeek);
+  }
+
+  getWeekDates(data: DateTime<true>): string[] {
+    return this.weekDates = Array.from({ length: 7 }, (_, i) =>
+      data.plus({ days: i }).toFormat('ccc, dd') // Format as desired
     );
+  }
+  checkMonth(value: string): string{
+    return this.timeFrame.month === value ? this.timeFrame.month : value;
+  }
+  checkYear(value: number): number {
+    return this.timeFrame.year === value ? this.timeFrame.year : value;
+  }
+  incrementDailyTime(startHour: number, endHour: number): string[] {
+    // Initialize the starting time (06:00 AM)
+    let currentTime = new Date();
+    currentTime.setHours(startHour, 0, 0, 0); // Set hours, minutes, seconds, and milliseconds
+
+    const times = [];
+
+    // Loop until the end hour is reached
+    while (currentTime.getHours() <= endHour) {
+      // Format the current time as HH:mm
+      const formattedTime = currentTime.toTimeString().slice(0, 5);
+      times.push(formattedTime);
+
+      // Increment the time by one hour
+      currentTime.setHours(currentTime.getHours() + 1);
+    }
+
+    return times;
   }
 }
