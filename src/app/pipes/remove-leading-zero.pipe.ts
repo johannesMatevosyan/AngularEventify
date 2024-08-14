77aae227ed8e5ee62051bbaa520bb07555142a1c
Hovhannes Matevosyan <net.hovhannes.matevosyan@mhp.com>
2024-08-14 20:40:58 +0400

    modified removeZerosPipe custom pipe to handle zeros only at the end

--- src/app/pipes/remove-leading-zero.pipe.ts
+++ src/app/pipes/remove-leading-zero.pipe.ts
@@ -6,10 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class removeZerosPipe implements PipeTransform {
   transform(value: string): string {
-    // Check if the value starts with '0' and remove it
-    if (value.startsWith('0')) {
-      value = value.substring(1);
-    }
     const  shortenToDate = (longDate : string) => longDate.split(':')[0];
     return shortenToDate(value);
   }
