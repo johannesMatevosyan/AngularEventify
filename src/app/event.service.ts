52c33ae6d651a50d18385bd89c6c5ba59c9122fd
Hovhannes Matevosyan <net.hovhannes.matevosyan@mhp.com>
2024-08-16 20:42:21 +0400

    added new events

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -3,54 +3,90 @@ import { Observable, of } from 'rxjs';
 
 
 export interface IEvent {
-  name: string;
-  start: string;
-  end: string;
+  title: string;
+  time: string;
+  endTime: string;
   date: string;
-  day: string;
   desciption: string;
 }
 
 export const EVENTS: IEvent[] = [
   {
-    name: "Group Meeting",
-    start: "13:30",
-    end: "14:00",
+    title: "Celebrity Meeting",
+    time: "15:30",
+    endTime: "16:30",
+    date: "2024-07-16",
+    desciption: "Celebrity Meeting is a meeting of the Celebrity members",
+  },
+  {
+    title: "Staff Meeting",
+    time: "13:30",
+    endTime: "14:00",
+    date: "2024-07-27",
+    desciption: "Staff Meeting is a meeting of the Staff members",
+  },
+  {
+    title: "Group Meeting",
+    time: "13:30",
+    endTime: "14:00",
     date: "2024-07-29",
-    day: "Thursday",
     desciption: "Group Meeting is a meeting of the Group members",
   },
   {
-    name: "Board Meeting",
-    start: "08:30",
-    end: "09:30",
-    date: "2024-08-04",
-    day: "Thursday",
-    desciption: "Board Meeting is a meeting of the board of directors of an organization, held to discuss various matters.",
+    title: "Breakfast",
+    time: "08:00",
+    endTime: "10:00",
+    date: "2024-08-12",
+    desciption: "Breakfast is the first meal of the day eaten in the morning.",
   },
   {
-    name: "Hot Yoga",
-    start: "08:00",
-    end: "09:30",
-    date: "2024-08-12",
-    day: "Monday",
+    title: "Hot Yoga",
+    time: "08:00",
+    endTime: "09:30",
+    date: "2024-08-13",
     desciption: "Hot Yoga is a series of yoga poses done in a heated room. The heat allows for a deeper stretch and helps to detoxify the skin. The room will be heated to 105°F with 40% humidity. Please bring a towel and water and be prepared to sweat.",
   },
   {
-    name: "Board Meeting",
-    start: "13:30",
-    end: "14:00",
+    title: "Acro Yoga",
+    time: "15:00",
+    endTime: "18:00",
+    date: "2024-08-14",
+    desciption: "Hot Yoga is a series of yoga poses done in a heated room. The heat allows for a deeper stretch and helps to detoxify the skin. The room will be heated to 105°F with 40% humidity. Please bring a towel and water and be prepared to sweat.",
+  },
+  {
+    title: "exercises",
+    time: "16:00",
+    endTime: "16:30",
+    date: "2024-08-15",
+    desciption: "exercises is a series of yoga poses done in a heated room. The heat allows for a deeper stretch and helps to detoxify the skin. The room will be heated to 105°F with 40% humidity. Please bring a towel and water and be prepared to sweat.",
+  },
+  {
+    title: "Board Meeting ",
+    time: "13:00",
+    endTime: "14:00",
     date: "2024-08-16",
-    day: "Thursday",
     desciption: "Board Meeting is a meeting of the board of directors of an organization, held to discuss various matters.",
   },
   {
-    name: "Hollyday",
-    start: "12:00",
-    end: "14:00",
+    title: "Coding practice",
+    time: "13:00",
+    endTime: "14:00",
+    date: "2024-08-16",
+    desciption: "Coding practice is a practice of coding",
+  },
+  {
+    title: "Sunday fun",
+    time: "08:00",
+    endTime: "09:30",
+    date: "2024-08-18",
+    desciption: "lorem ipsum",
+  },
+  {
+    title: "Hollyday",
+    time: "12:00",
+    endTime: "14:00",
     date: "2024-08-22",
-    day: "Thursday",
-    desciption: "Board Meeting is a meeting of the board of directors of an organization, held to discuss various matters.",
+    desciption: "Hollyday is a day of festivity or recreation when no work is done.",
   }
 ];
 
