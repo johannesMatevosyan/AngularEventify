8d643fbbbdefc8cf679abe40e202152523b60290
Hovhannes Matevosyan <net.hovhannes.matevosyan@mhp.com>
2024-08-17 21:04:53 +0400

    created modal component

---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent {
+
+}
