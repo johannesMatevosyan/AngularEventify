c1572f1a27a8a2f47cabd65ca57fb08eeb526178
Hovhannes Matevosyan <net.hovhannes.matevosyan@mhp.com>
2024-08-18 17:04:24 +0400

    added methods to modal service

--- src/app/service/modal.service.ts
+++ src/app/service/modal.service.ts
@@ -4,6 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ModalService {
+  private modals: any[] = [];
 
-  constructor() { }
+  add(modal: any): void {
+    this.modals.push(modal);
+  }
+
+  remove(id: string): void {
+    this.modals = this.modals.filter(x => x.id !== id);
+  }
+
+  open(id: string): void {
+    const modal = this.modals.find(x => x.id === id);
+    modal.open();
+  }
+
+  close(id: string): void {
+    const modal = this.modals.find(x => x.id === id);
+    modal.close();
+  }
 }
