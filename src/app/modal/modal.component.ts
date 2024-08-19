152b1223ac07292add6cb75b7820897a3eff1999
Hovhannes Matevosyan <net.hovhannes.matevosyan@mhp.com>
2024-08-19 17:00:26 +0400

    modified features to detect escape key event

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EventManager } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-modal',
@@ -8,9 +9,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+  @Input() hideOnEsc: boolean = true;
   @Input() title: string = '';
   isVisible: boolean = false;
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    // detect if ESC key button was pressed
+    this.renderer.listen('window', 'keyup.esc', () => {
+      if (this.hideOnEsc) {
+        this.close();
+      }
+    });
+  }
 
   open(): void {
     this.isVisible = true;
