b258c21587d1efff5a09bf99c03d9163f4ec86d5
Hovhannes Matevosyan <net.hovhannes.matevosyan@mhp.com>
2024-09-08 19:25:33 +0400

    feat:: import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
@@ -28,7 +28,7 @@ import { DisableRightClickDirective } from './directives/disable-right-click.dir
     BrowserModule,
     AppRoutingModule,
     ModalDialogComponent,
-
+    HttpClientModule,
     DragDropModule
   ],
   providers: [],
