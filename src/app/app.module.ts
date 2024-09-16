38638130e21e5356f05c8a4dd935aaea1b1f89c0
Hovhannes Matevosyan <net.hovhannes.matevosyan@mhp.com>
2024-09-16 17:54:08 +0400

    refactor:: removed temporary routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { CalendarHeaderComponent } from './calendar/calendar-header/calendar-header.component';
@@ -22,7 +21,6 @@ import { ApplicationPipesModule } from './application-pipes/application-pipes.mo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ModalDialogComponent,
     HttpClientModule,
     ApplicationPipesModule
