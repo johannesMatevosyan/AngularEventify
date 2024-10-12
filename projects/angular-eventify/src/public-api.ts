839be9966ceb34e132ea2415395a08b90b45944c
Hovhannes Matevosyan <net.hovhannes.matevosyan@mhp.com>
2024-10-12 21:23:18 +0400

    feat:: removed test library project

--- projects/angular-eventify/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-eventify
- */
-
-export * from './lib/angular-eventify.service';
-export * from './lib/angular-eventify.component';
-export * from './lib/angular-eventify.module';
